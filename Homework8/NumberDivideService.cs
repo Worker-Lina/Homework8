a5e0a5a0333ae8574859482eed871a4ccb1b583f
алина человечкова <alinasmart19@mail.ru>
2020-12-28 21:29:12 +0600

    Добавьте файлы проекта.

---
+++ Homework8/NumberDivideService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Homework8
+{
+    /// <summary>
+    /// Класс для деления двух чисел
+    /// </summary>
+    public class NumberDivideService
+    {
+        /// <summary>
+        /// Метод для деления двух чисел
+        /// </summary>
+        /// <param name="numerator">Числитель</param>
+        /// <param name="denominator">Знаменатель</param>
+        /// <returns>Int</returns>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="ArgumentException"/> 
+
+
+        public int NumberDivide(int numerator, int denominator)
+        {
+            if (numerator == null || denominator == null)
+            {
+                throw new ArgumentNullException("Было передано пустое значение");
+            }
+            else if (denominator == 0)
+            {
+                throw new ArgumentException("На ноль делить нельзя!");
+            }
+            return numerator / denominator;
+        }
+
+
+
+    }
+}
---
+++ Homework8/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Homework8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //---------1---------------
+
+            /*var service = new NumberDivideService();
+            try
+            {
+                Console.WriteLine(service.NumberDivide(35, 5));
+            }
+            catch (ArgumentNullException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }*/
+
+
+
+            //-------------2-------------
+
+            Random random = new Random();
+            int defaultArraySize = 5;
+
+            Console.WriteLine("Введите размер массива");
+            if (!int.TryParse(Console.ReadLine(), out int arraySize))
+            {
+                arraySize = defaultArraySize;
+            }
+
+
+            int[] someArray = new int[arraySize];
+            Console.WriteLine("Введите индекс элемента массива");
+            int index = int.Parse(Console.ReadLine());
+
+
+            try
+            {
+                someArray[index] = random.Next(0, 100);
+            }
+            catch(IndexOutOfRangeException exception)
+            {
+                Console.WriteLine($"{ exception.Message}\nБыл изменен последний элемент массива");
+            }
+            finally
+            {
+                someArray[someArray.Length-1] = random.Next(0, 100);
+                Console.WriteLine("Выполнение завершено");
+            }
+
+
+
+        }
+    }
+}
